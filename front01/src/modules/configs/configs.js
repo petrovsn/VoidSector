195f952dcc63e78623015a28f23cc29d53e69b19
petrovsn <petrovsnwm@gmail.com>
2024-12-24 12:43:16 +0300

    complete assembly including node js files

--- front01/src/modules/configs/configs.js
+++ front01/src/modules/configs/configs.js
@@ -3,8 +3,8 @@ export const ip = "localhost"
 //export const ip = "192.168.1.4"
 
 export function is_local(){
- //return false
- return ip==="localhost"
+ return false
+ //return ip==="localhost"
 }
 
 
